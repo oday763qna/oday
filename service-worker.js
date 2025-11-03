73b42b1719d3fadcb6678bd94704ce5e55a93528
oday763qna <oday5qutqut@gmail.com>
2025-11-03 14:57:32 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,74 @@
+// A simple service worker for caching static assets
+
+const CACHE_NAME = 'fitness-planner-cache-v1';
+// Add the paths to the files you want to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Add other assets like CSS, JS, images if they are not loaded from a CDN
+  // '/styles/main.css',
+  // '/app.js',
+  // '/icon-192x192.png',
+  // '/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Not in cache - fetch from network
+        return fetch(event.request).then(
+          response => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
